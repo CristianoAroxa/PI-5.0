0327154bfc702690480da4754f387d4a6cec86da
CristianoAroxa <cristiano.aroxa@hotmail.com>
2024-11-13 22:12:08 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const questionsRoute = require('./routes/questions');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+//Middleware para cors
+app.use(cors());
 
 // Middleware para parsear JSON
 app.use(bodyParser.json());
